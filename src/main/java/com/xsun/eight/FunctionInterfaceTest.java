7d25831418c3b309dcaead840cf55856de0a9154
sunhaoran03 <sunhaoran03@meituan.com>
2019-01-21 21:20:46 +0800

    first commit

---
+++ src/main/java/com/xsun/eight/FunctionInterfaceTest.java
@@ -0,0 +1,11 @@
+package com.xsun.eight;
+
+/**
+ * created at 下午8:33, 2019/1/21
+ *
+ * @author sunhaoran
+ */
+@FunctionalInterface
+public interface FunctionInterfaceTest {
+    String accept(String hello) ;
+}
