412b7377933ff1ba657aaecff1dedd7f2ea857fb
dynamic-sevn <nuaa_sunhr@yeah.net>
2019-01-28 17:08:34 +0800

    fix gradle

--- src/main/java/com/xsun/eight/Main.java
+++ src/main/java/com/xsun/eight/Main.java
@@ -1,5 +1,10 @@
 package com.xsun.eight;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * created at 上午11:30, 2019/1/22
  *
@@ -11,5 +16,12 @@ public class Main {
 
         // hello 仅占位，可以理解为形参
         demo.test(hello -> "function " + hello);
+        demo.test(hello -> "[function]" + hello);
+        final List<Integer> finalList = new ArrayList<>() ;
+        finalList.add(5);
+        System.out.println(finalList);
+
+        System.out.println(LocalDateTime.now().withHour(23).withMinute(59).withSecond(59));
+        System.out.println(LocalDateTime.now().withHour(14).withMinute(55).format(DateTimeFormatter.ofPattern("HH:mm")));
     }
 }
