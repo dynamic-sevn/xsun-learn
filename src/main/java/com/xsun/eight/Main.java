7755f1b42dbac83749a6feaec91ace65b51bc162
dynamic-sevn <nuaa_sunhr@yeah.net>
2019-01-31 10:13:25 +0800

    m

--- src/main/java/com/xsun/eight/Main.java
+++ src/main/java/com/xsun/eight/Main.java
@@ -1,9 +1,13 @@
 package com.xsun.eight;
 
+import com.alibaba.fastjson.JSON;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * created at 上午11:30, 2019/1/22
@@ -23,5 +27,8 @@ public class Main {
 
         System.out.println(LocalDateTime.now().withHour(23).withMinute(59).withSecond(59));
         System.out.println(LocalDateTime.now().withHour(14).withMinute(55).format(DateTimeFormatter.ofPattern("HH:mm")));
+
+        System.out.println(JSON.toJSONString(new Date()));
+        System.out.println(UUID.randomUUID());
     }
 }
